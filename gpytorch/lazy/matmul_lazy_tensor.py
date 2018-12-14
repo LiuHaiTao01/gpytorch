[WIP] Fixing complex test derivatives

gpytorch/lazy/matmul_lazy_tensor.py
@@ -83,6 +83,6 @@ class MatmulLazyTensor(LazyTensor):
         else:
             return super(MatmulLazyTensor, self).diag()
 
-    @cached
+    # @cached
     def evaluate(self):
         return torch.matmul(self.left_lazy_tensor.evaluate(), self.right_lazy_tensor.evaluate())

gpytorch/lazy/root_lazy_tensor.py
@@ -77,7 +77,7 @@ class RootLazyTensor(LazyTensor):
         else:
             return super(RootLazyTensor, self).diag()
 
-    @cached
+    # @cached
     def evaluate(self):
         eval_root = self.root.evaluate()
         return torch.matmul(eval_root, eval_root.transpose(-1, -2))

gpytorch/models/exact_prediction_strategies.py
@@ -71,7 +71,7 @@ class DefaultPredictionStrategy(object):
         return test_train_covar.matmul(precomputed_cache)
 
     @property
-    @cached(name="mean_cache")
+    # @cached(name="mean_cache")
     def mean_cache(self):
         train_mean = self.train_mean
         train_labels = self.train_labels
@@ -104,7 +104,7 @@ class DefaultPredictionStrategy(object):
         return mean_cache.detach()
 
     @property
-    @cached(name="covar_cache")
+    # @cached(name="covar_cache")
     def covar_cache(self):
         train_train_covar = self.likelihood(
             MultivariateNormal(torch.zeros(1), self.train_train_covar), self.train_inputs